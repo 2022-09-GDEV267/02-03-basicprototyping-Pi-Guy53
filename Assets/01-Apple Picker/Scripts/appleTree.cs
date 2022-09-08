b3440b1fd7df9005d0a5774b2b554012d212c4bd
Pi-Guy53 <71029215+Pi-Guy53@users.noreply.github.com>
2022-09-08 12:25:20 -0400

    tree movement and random directions

---
+++ Assets/01-Apple Picker/Scripts/appleTree.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class appleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePref;
+
+    public float speed;
+    public float leftRightBounds;
+    public float changeDirChance;
+    public float appleDropRate;
+
+    private Vector3 pos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -leftRightBounds)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if(pos.x >leftRightBounds)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+        else if(Random.value < changeDirChance)
+        {
+            speed *= -1;
+        }
+    }
+}
