5353e7060f6bdc4cbc74e193c00b0023dec7f63e
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2022-09-10 10:07:18 -0400

    added the score counter, implementation hurts, will revisit after tutorial is complete

--- Assets/01-Apple Picker/Scripts/basket.cs
+++ Assets/01-Apple Picker/Scripts/basket.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreTxt;
+
     Vector3 mousePos2D;
     Vector3 mousePos3D;
     Vector3 pos;
+
+    private void Start()
+    {
+        GameObject scoreGo = GameObject.Find("score");
+        scoreTxt = scoreGo.GetComponent<Text>();
+        scoreTxt.text = "0";
+    }
 
     private void Update()
     {
@@ -25,6 +36,11 @@ public class basket : MonoBehaviour
         if(coll.collider.CompareTag("apple"))
         {
             Destroy(coll.collider.gameObject);
+
+            int score = int.Parse(scoreTxt.text);
+            score += 100;
+
+            scoreTxt.text = score.ToString();
         }
     }
 }
