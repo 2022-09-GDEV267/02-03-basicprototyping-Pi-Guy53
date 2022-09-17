9581be9fcab3eb5f6572566ac70c83a945edbbe0
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2022-09-17 11:21:18 -0400

    added slingshot and projectile

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float velocityMulti;
+
+    public Transform launchPoint;
+    private Vector3 launchPos;
+
+    private bool aimingMode;
+
+    private GameObject thisProjectile;
+    private Rigidbody projectileRb;
+
+    float maxMagnitude;
+
+    private Vector3 mousePos2D, mousePos3D, mouseDelta;
+
+    private void Start()
+    {
+        launchPos = launchPoint.position;
+
+        launchPoint.gameObject.SetActive(false);
+        maxMagnitude = GetComponent<SphereCollider>().radius;
+    }
+
+    private void Update()
+    {
+        if (aimingMode)
+        {
+            mousePos2D = Input.mousePosition;
+            mousePos2D.z = -Camera.main.transform.position.z;
+            mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+            mouseDelta = mousePos3D - launchPos;
+
+            if (mouseDelta.magnitude > maxMagnitude)
+            {
+                mouseDelta.Normalize();
+                mouseDelta *= maxMagnitude;
+            }
+
+            thisProjectile.transform.position = launchPos + mouseDelta;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                aimingMode = false;
+                projectileRb.isKinematic = false;
+                projectileRb.velocity = -mouseDelta * velocityMulti;
+
+                thisProjectile = null;
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        launchPoint.gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        launchPoint.gameObject.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+
+        thisProjectile = Instantiate(projectilePrefab, launchPoint.position, launchPoint.rotation);
+
+        projectileRb = thisProjectile.GetComponent<Rigidbody>();
+        projectileRb.isKinematic = true;
+    }
+
+}
