2eca0b08398e87930552b82b3ef90e252ab81460
Pi-Guy53 <71029215+Pi-Guy53@users.noreply.github.com>
2022-09-22 13:36:44 -0400

    initial mash up setup

---
+++ Assets/03-Prototype1/_scripts/playerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private int x, y;
+
+    public GameObject basePiece;
+    public GameObject body;
+    public GameObject head;
+
+    void Update()
+    {
+        
+    }
+}
