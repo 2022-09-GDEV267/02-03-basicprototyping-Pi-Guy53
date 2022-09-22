e8071783b2ed9f52fa50acb74b3de941b802b47c
Pi-Guy53 <71029215+Pi-Guy53@users.noreply.github.com>
2022-09-22 16:12:30 -0400

    modified dalek, started work on character controller

--- Assets/03-Prototype1/_scripts/playerController.cs
+++ Assets/03-Prototype1/_scripts/playerController.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    private int x, y;
+    private float x, y;
+
+    public float speed;
+    public float jumpTime;
 
     public GameObject basePiece;
     public GameObject body;
     public GameObject head;
 
+    public GameObject weapon;
+
+    private GameObject eye;
+
+    private Rigidbody rb;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        eye = new GameObject("eye");
+        eye.transform.position = transform.position;
+        eye.transform.parent = transform;
+    }
+
     void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        direction = transform.forward * y + transform.right * x;
+
+        rb.AddForce(direction.normalized * speed);
     }
 }
